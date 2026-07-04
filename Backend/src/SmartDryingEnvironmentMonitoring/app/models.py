f6ecb5c5f99bf974679c4835a1183ce9f3e2cd5b
Milan sanjaya <sanjaya2170@gmail.com>
2026-07-04 12:40:19 +0530

    update

--- Backend/src/SmartDryingEnvironmentMonitoring/app/models.py
+++ Backend/src/SmartDryingEnvironmentMonitoring/app/models.py
@@ -3,3 +3,18 @@ from pydantic import BaseModel
 
 class CommandRequest(BaseModel):
     command: str
+
+
+class SensorData(BaseModel):
+    temperature: float | None = None
+    humidity: float | None = None
+    ds_temperature: float | None = None
+    gas: int | None = None
+
+    # HX711
+    raw_weight: int | None = None
+    weight: float | None = None
+
+    heater: bool = False
+    light: bool = False
+    fan: bool = False
